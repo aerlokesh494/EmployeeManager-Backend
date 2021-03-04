311753885661bfa6d158cc54148b7adcfc08a7f6
lokesh <aerlokesh1@gmail.com>
2021-03-04 09:28:00 +0530

    hopefully final commit

--- src/main/java/lokesh/aer/employeemanager/service/EmployeeService.java
+++ src/main/java/lokesh/aer/employeemanager/service/EmployeeService.java
@@ -5,11 +5,13 @@ import lokesh.aer.employeemanager.model.Employee;
 import lokesh.aer.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+
 
 import java.util.List;
 import java.util.UUID;
-
 @Service
+@Transactional
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
 
@@ -18,22 +20,22 @@ public class EmployeeService {
         this.employeeRepo = employeeRepo;
     }
 
-    public Employee addEmployee(Employee employee){
+    public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
-        return  employeeRepo.save(employee);
-    }
-
-    public List<Employee> findAllEmployees(){
-        return employeeRepo.findAll();
-    }
-
-    public Employee updateEmployee(Employee employee){
         return employeeRepo.save(employee);
     }
 
-    public Employee findEmployeeById(Long id){
-        return employeeRepo.findEmployeeById(id).orElseThrow(()->new UserNotFoundException("user by id" +
-                id+" was not found"));
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
     }
 
     public void deleteEmployee(Long id){
